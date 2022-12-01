16ba824b239283b34accc4ae2d5b0f518755c1af
Arsenii Ohar <arsen.ogar@gmail.com>
2022-12-01 14:24:02 +0200

    lab-9

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 import sys
+
 sys.path = ['', '..'] + sys.path[1:]
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from models.models import *
+from models.models import Base
 
 from alembic import context
 
@@ -20,6 +21,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 target_metadata = Base.metadata
+
+
 # target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
